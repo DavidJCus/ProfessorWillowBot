8e71fd82814ad259e6731329e96f4ae7ad8fc8c3
DavidJCus <91638643+DavidJCus@users.noreply.github.com>
2021-11-04 15:04:33 -0400

    delete banned_words

--- banned_word.py
+++
@@ -1,5 +0,0 @@
-#def initialize():
-banned_words = ["gay","g@y", "homo","h0m0", "fag", "faggot", "pussy", "nigger", "chink", "puta", "putain","dick", "bitch", "rape", "r@pe", "whore"]
-banned_phrases = ["suck my dick", "fuck you", "fucked your mom", "fucked ur mom", "beta cuck", "love you long time", "ching chong"]
-smh_words = ["ligma", "deez", "updog"]
-smh_phrases = ["deez nuts"]
